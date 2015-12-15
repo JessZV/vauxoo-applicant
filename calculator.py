bbf3a0ca09d05f8e57f1bc4a71226a8ce531e412
JessZV <jessy_080391@hotmail.com>
2015-12-15 12:44:38 -0600

    Update calculator.py

--- calculator.py
+++ calculator.py
@@ -4,13 +4,15 @@ Your module documentation here
 
 
 class CalculatorClass(object):
-    """
-    Your class documentation here
-    """
 
-    def sum(self, num_list):
-        """
-        Your method documentation here
-        """
-        # your sum code here
-        return "not implement yet"  # Remove this dummy line
+    def sum(self, n):
+	i = 0
+	while n != 0:
+		i = n + i
+		n += False
+		
+	if suma(n) =! 0:
+	print("El resultado es ", n)
+	
+		if n == 0:
+		break
