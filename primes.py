768b1b7553370c963098bff011876ea5fab39969
JessZV <jessy_080391@hotmail.com>
2015-12-15 12:21:42 -0600

    Update primes.py

--- primes.py
+++ primes.py
@@ -1,14 +1,12 @@
-"""
-Your module documentation here
-"""
+
 
 
 class PrimeClass(object):
-    def is_prime(numero):
+    def is_prime(numero): #definicion de la funcion 
         
-    for i in range(2,numero):
+    for i in range(2,numero): 
 
-        if (numero%i)==0:
+        if (numero%i)==0: si el modulo del contador es 0 enntocnes no es primo
 
             
             return False
@@ -16,22 +14,22 @@ class PrimeClass(object):
     return True
 
 bandera = False 
-
-while True:
+3
+while True: mientras sea verdadero
 
     try:
 
-        numero = int(raw_input("inserta un numero (0) salir >> "))
+        numero = int(raw_input("inserta un numero (0) salir >> ")) #si agrega un 0 saldra de la ejecucion
 
         if numero==0:
 
             break
 
-        if is_prime(numero):
+        if is_prime(numero): #manda llamar la funcion para saber notificar si el numero es primo
         	bandera = True            
         	print  bandera 
 
-        else:
+        else:            #si no lo es.
 	       bandera = False
                print  bandera 
 
